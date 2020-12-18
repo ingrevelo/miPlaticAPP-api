0af8345a3df3e933eb9901fd6a5647096f146d2a
Cesar Revelo <ing.revelo@gmail.com>
2020-12-17 22:04:46 -0500

    Update main

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ api = FastAPI()
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
     "http://localhost", "http://localhost:8080",
-    "https://miplaticaap-app.herokuapp.com/"
+    "https://miplaticaap-app.herokuapp.com"
 ]
 
 api.add_middleware(
